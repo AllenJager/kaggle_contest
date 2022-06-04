b96dfa659608493c85bf6b59fa72b9f6164272b5
ShinelNozen <53746318+AllenJager@users.noreply.github.com>
2022-06-04 12:19:22 +0800

    Add files via upload
    今年四五月份结束的kaggle竞赛，做个总结作为深度学习课程的课程作业

---
+++ G-search/lab_info/log.md
@@ -0,0 +1,8 @@
+![image-20211130170805095](C:\Users\cultivator\Desktop\G-search\debug_info\image-20211130170805095.png)
+
+原始train+（初始的特征工程）的数据概览
+
+
+
+
+
---
+++ G-search/src/reference/main.py
@@ -0,0 +1,8 @@
+print("hello")
+Train_Data = {'A':{'i1':1,'i2':1 ,'i4':1},
+     'B':{'i2':1,'i4':1},
+     'C':{'i1':1,'i2':1,'i5':1},
+     'D':{'i2':1,'i3':1}}
+for key in Train_Data:
+    print(key)
+    
